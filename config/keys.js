12d360e9be124b049e5b7f04570cb11f900f9373
an-nguyen239 <anguyen108@student.gsu.edu>
2019-02-23 23:54:57 -0500

    Create JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	mongoURI: 
-	'mongodb://annguyen1:annguyen1@ds249035.mlab.com:49035/devnet'
+	'mongodb://annguyen1:annguyen1@ds249035.mlab.com:49035/devnet',
+	secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 //Load User model
-const User = require('../../models/User')
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -64,7 +66,22 @@ router.post('/login', (req, res) => {
 			bcrypt.compare(password, user.password)
 			.then(isMatch => {
 				if (isMatch) {
-					res.json({msg: 'Success'});
+					// User Matched
+
+					const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT Payload
+
+					// Sign Token
+
+					jwt.sign(
+						payload, 
+						keys.secretOrKey, 
+						{ expiresIn: 3600 }, 
+						(err, token) => {
+							res.json ({
+								success: true,
+								token: 'Bearer ' + token
+							})
+					});
 				} else {
 					return res.status(400).json({password: "Password incorrect"});
 				}
