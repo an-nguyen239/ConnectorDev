8e33594949396f8b99cb173330a0824df3af1976
an-nguyen239 <anguyen108@student.gsu.edu>
2019-03-12 01:35:02 -0400

    finish login/logout

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import store from './store';
 
 import NavBar from './components/layout/Navbar';
@@ -15,15 +15,25 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
-//Check for token
+// Check for token
 if (localStorage.jwtToken) {
-  //Set auth token header auth
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  //Decode token and get user info and exp
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
 
-  //Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded)); 
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
